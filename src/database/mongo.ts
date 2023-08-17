19304d802e04811453ba76c34bef511b7df804d3
wildanjisung <wildanjisung@gmail.com>
2023-08-18 00:53:04 +0700

    [modify] run mongo container use docker

--- src/database/mongo.ts
+++ src/database/mongo.ts
@@ -1,6 +1,7 @@
 import { MongooseModule } from '@nestjs/mongoose';
 
 export const databaseProviders = [
-  MongooseModule.forRoot('mongodb://mongodb:27017/nest-mongodb-app', {
+  // MongooseModule.forRoot('mongodb://mongodb:27017/nest-mongodb-app', {
+  MongooseModule.forRoot('mongodb://user:pass@localhost:27017/?authMechanism=DEFAULT', {
   }),
 ];
